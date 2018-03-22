00360380381eacd1f284e9889faa5c81ec9427bc
Darshan Mistry <mistrydarshan99@gmail.com>
2018-03-22 14:35:10 +0530

    QA commit

--- app/src/main/java/com/insiderapp/MainActivity.java
+++ app/src/main/java/com/insiderapp/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     Log.e(TAG, "onCreate: -----------first line");
+    Log.e(TAG, "onCreate: -----------QA line");
   }
 }
